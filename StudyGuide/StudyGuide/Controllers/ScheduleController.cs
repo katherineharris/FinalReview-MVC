d7adda0313afbeb4ff6c9c9af6c97dd83b33b50a
Katie Harris <katherineharris@ou.edu>
2019-05-01 14:12:11 -0500

    kh

---
+++ StudyGuide/StudyGuide/Controllers/ScheduleController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudyGuide.Models;
+
+namespace StudyGuide.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            return View(db.FootballSchedules.ToList());
+        }
+
+        // GET: Schedule/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FootballSchedule footballSchedule = db.FootballSchedules.Find(id);
+            if (footballSchedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(footballSchedule);
+        }
+
+        // GET: Schedule/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schedule/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Season,Opponent,Date,IsHomeGame")] FootballSchedule footballSchedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FootballSchedules.Add(footballSchedule);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(footballSchedule);
+        }
+
+        // GET: Schedule/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FootballSchedule footballSchedule = db.FootballSchedules.Find(id);
+            if (footballSchedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(footballSchedule);
+        }
+
+        // POST: Schedule/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Season,Opponent,Date,IsHomeGame")] FootballSchedule footballSchedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(footballSchedule).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(footballSchedule);
+        }
+
+        // GET: Schedule/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FootballSchedule footballSchedule = db.FootballSchedules.Find(id);
+            if (footballSchedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(footballSchedule);
+        }
+
+        // POST: Schedule/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FootballSchedule footballSchedule = db.FootballSchedules.Find(id);
+            db.FootballSchedules.Remove(footballSchedule);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
